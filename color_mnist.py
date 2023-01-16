413597ccec118bd4a8eab16c8d27bef7fbbffb04
RoiPapo <thedarkhermit@gmail.com>
2023-01-16 10:00:21 +0200

    trainable

--- color_mnist.py
+++ color_mnist.py
@@ -1,44 +1,121 @@
+import random
 from typing import Optional
-
+from random import randint
+import torch
+from PIL import Image
+import PIL
+from matplotlib import pyplot as plt
 from torchvision import datasets, transforms
 from torchvision.datasets import MNIST
+import numpy as np
+
+import visualize
+from Trainer import Trainer
+
+from visualize import Visualizer as Viz
+from torch import optim
 from typing import Callable, Optional
+
+colors = {
+    0: [255, 0, 0],
+    1: [0, 255, 0],
+    2: [0, 0, 255],
+    3: [255, 128, 0],
+    4: [255, 0, 255],
+    5: [128, 128, 128],
+    6: [128, 0, 255],
+    7: [0, 255, 255],
+    8: [255, 255, 0],
+    9: [255, 0, 128]
+}
+
+from models import VAE
+
+
+
+
+class color(object):
+    def __call__(self, img):
+        """
+        :param img: (PIL): Image
+        :return: ycbr color space image (PIL)
+        """
+        img = np.asarray(img)
+        img = (img - np.min(img)) / (np.max(img) - np.min(img))
+        chosen_color = colors[randint(0, 9)]
+        img2 = np.zeros((32, 32, 3))
+        img2[:, :, 0] = img * chosen_color[0]
+        img2[:, :, 1] = img * chosen_color[1]
+        img2[:, :, 2] = img * chosen_color[2]
+        # img2 = np.transpose(img2, (1, 2, 0)).astype(int)
+
+        return img2.astype(np.uint8)
+    #
+    # def __repr__(self):
+    #     return self.__class__.__name__ + '()'
 
 
 def wrapper(x):
-    class _wrap(x):
+    class _wrap(MNIST):
         def __init__(
                 self,
                 root: str,
                 train: bool,
-                target_transform: Optional[Callable] = None
+
         ) -> None:
-            transform = transforms.Compose([
+            transforme = transforms.Compose([
+                transforms.Resize(32),
+                color(),
                 transforms.ToTensor(),
-                transforms.Normalize((0.5,), (0.5,))
+                # transforms.Normalize((0.5,), (0.5,))
             ])
             download = True if train else False
-            self.wrap = MNIST(root, train, transform, target_transform, download)
+            super().__init__(root, train, transforme, download=download)
+
+
 
     return _wrap
 
 
 if __name__ == '__main__':
-    transform_obj = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-    ])
     ColorMNISTDataset = wrapper(datasets.MNIST)
     train_ColorMNISTDataset = ColorMNISTDataset(root='./data/', train=True)
     test_ColorMNISTDataset = ColorMNISTDataset(root='./data/', train=False)
+    train_loader = torch.utils.data.DataLoader(dataset=train_ColorMNISTDataset,
+                                               batch_size=64,
+                                               shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_ColorMNISTDataset,
+                                              batch_size=64,
+                                              shuffle=True)
 
-    train_dataset = datasets.MNIST(root='./data/',
-                                   train=True,
-                                   transform=transform_obj,
-                                   download=True)
+    # Build a dataloader for your data
+    # dataloader = get_my_dataloader(batch_size=32)
+    dataloader = train_loader
+    # Define latent distribution
+    latent_spec = {'cont': 20, 'disc': [10, 5, 5, 2]}
 
-    test_dataset = datasets.MNIST(root='./data/',
-                                  train=False,
-                                  transform=transform_obj,
-                                  download=True)
-    print("moo")
+    # Build a Joint-VAE model
+    model = VAE(img_size=(3, 32, 32), latent_spec=latent_spec)
+
+    # Build a trainer and train model
+    from torch.optim import Adam
+    optimizer = Adam(model.parameters())
+    trainer = Trainer(model, optimizer,
+                      cont_capacity=[0., 5., 25000, 30.],
+                      disc_capacity=[0., 5., 25000, 30.])
+    trainer.train(dataloader, epochs=7)
+
+    # Visualize samples from the model
+    viz = Viz(model)
+    samples = viz.samples()
+    a= viz.latent_traversal_line()
+    b= viz.latent_traversal_grid()
+    c= viz.all_latent_traversals()
+
+    traversals = viz.latent_traversal_grid(cont_idx=2, cont_axis=1, disc_idx=0, disc_axis=0, size=(10, 10))
+    ordering = [9, 3, 0, 5, 7, 6, 4, 8, 1, 2]  # The 9th dimension corresponds to 0, the 3rd to 1 etc...
+    traversals = visualize.reorder_img(traversals, ordering, by_row=True)
+
+    # plt.imshow(traversals.numpy())
+
+
